f610af74e9b9da54ba24a89ec3efbdf5c5c05d4b
Bahram <baxram1997007@gmail.com>
2023-04-17 18:45:23 +0400

    New version)

--- OmdbApp_MVP/Presenter/Presenter.cs
+++ OmdbApp_MVP/Presenter/Presenter.cs
@@ -6,14 +6,14 @@ namespace OmdbApp_MVP
     internal class Presenter
     {
         private readonly Movie? _movie; // Model
-        private readonly SearchResult? searchResult; // Model
+        private readonly SearchResult? _searchResult; // Model
         private readonly OmdbViewForm? _view; // View
 
         public Presenter(OmdbViewForm? view)
         {
             _view = view;
             _movie = new Movie();
-            searchResult = new SearchResult();
+            _searchResult = new SearchResult();
             _view!.AddHandler += AddHandler;
             _view!.RemoveHandler += RemoveHandler;
         }
@@ -25,7 +25,7 @@ namespace OmdbApp_MVP
 
         private void AddHandler(object? sender, EventArgs e)
         {
-            _view!.listBox1.DataSource = searchResult?.Search?.Select(x => $"{x.Title}").ToList();
+            _view!.listBox1.DataSource = _searchResult?.Search?.Select(x => $"{x.Title}").ToList();
         }
 
     }
